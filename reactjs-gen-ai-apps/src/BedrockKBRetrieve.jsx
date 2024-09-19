60eb9e2d66a495cab0ceac02e4490a6ec969933a
stephengibsonDE <stephen.gibson@dataelephant.com>
2024-09-19 12:53:02 -0600

    Update BedrockKBRetrieve.jsx

--- reactjs-gen-ai-apps/src/BedrockKBRetrieve.jsx
+++ reactjs-gen-ai-apps/src/BedrockKBRetrieve.jsx
@@ -35,7 +35,7 @@ export default () => {
         setMessages(prev => [...prev,{ role: "user", content: content }])
         const question = await getStandaloneQuestion({modelId:currentModelId, messages:messages,  question:  value})
         console.log("standalone question:", question)
-        setLLMResponse(msg => msg + `Entendí: <strong>${question}</strong><br/>`)
+        setLLMResponse(msg => msg + `Understood: <strong>${question}</strong><br/>`)
 
         const retriever = await getBedrockKnowledgeBaseRetriever(currentKb.value)
         const docs = await retriever.invoke(question)   
@@ -60,7 +60,7 @@ export default () => {
     return (
 
         <Container key={2} disableContentPaddings={false}
-            header={<Header variant="h2">Conversacion</Header>}>
+            header={<Header variant="h2">Conversation</Header>}>
 
             <SpaceBetween size="xs">
                 <BedrockKBLoader ref={childRef} key={1} />
@@ -79,7 +79,7 @@ export default () => {
                 </Box>
                 {
                     llmResponse !== "" ?
-                        <Container fitHeight header={<strong>Respuesta LLM</strong>}>
+                        <Container fitHeight header={<strong>LLM Response</strong>}>
                             <div dangerouslySetInnerHTML={{ __html: llmResponse }} />
                         </Container> :
                         null
@@ -95,7 +95,7 @@ export default () => {
                     autoFocus
                     disabled={loading}
                     inputMode="text" />
-                <Button fullWidth key={2} loading={loading} onClick={sendText} variant="primary" >Enviar</Button>
+                <Button fullWidth key={2} loading={loading} onClick={sendText} variant="primary" >Submit</Button>
             </SpaceBetween>
 
 
